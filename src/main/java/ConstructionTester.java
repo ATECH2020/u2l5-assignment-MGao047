8306235add0e4bf047f8beda1c84aac87b5d22b9
Mark Gao <mark.184748@nv.ccsd.net>
2020-09-25 17:34:55 +0000

    Not Done Yet  construction

--- src/main/java/ConstructionTester.java
+++ src/main/java/ConstructionTester.java
@@ -1,9 +1,22 @@
 import java.util.Scanner;
 
-public class ConstructionTester
-{
-    public static void main(String[] args)
-    {
-        // Start here!
-    }
+public class ConstructionTester {
+  public static void main(String[] args) {
+    double taxRate;
+    int numBoards;
+    int numWindows;
+   
+    Scanner theScanner = new scanner(System.in);
+
+    System.out.print("Enter the sales tax rate:");
+    taxRate = theScanner.nextDouble();
+    System.out.println("How many boards do you need?");
+    numBoards = theScanner.nextInt();
+    System.out.println("How many windows do you need?");
+    numWindows = theScanner.nextInt();
+
+
+     Construction theConstruction = new Construction(8.0,11.0,taxRate);
+
+  }
 }
